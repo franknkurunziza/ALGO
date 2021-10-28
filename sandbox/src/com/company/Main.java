1543b630252f0b7a09b14bb2777cb76b1b0612ef
Frank Nkurunziza <franknkurunzia@gmail.com>
2021-10-27 21:17:50 -0400

    copying arrays

--- sandbox/src/com/company/Main.java
+++ sandbox/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -37,5 +39,28 @@ public class Main {
 
             }
         }
+        // Copy 2d array into another an 2d array using loop
+        int[][] source = {
+                {1, 2, 3, 4},
+                {5, 6},
+                {0, 2, 42, -4, 5}
+        };
+        int[][] destination = new int[source.length][];
+        for (int i=0;i<destination.length;i++){
+            destination[i] = new int[source[i].length];
+            for (int j=0;j<destination[i].length;j++){
+                destination[i][j]=source[i][j];
+            }
+        }
+
+         System.out.println(Arrays.deepToString(destination));
+        System.out.println("-----------------------------------------");
+        // Copy 2d array into another an 2d array using ArrayToCopy
+
+        for(int i=0;i< source.length;i++){
+            destination[i] = new int[source[i].length];
+            System.arraycopy(source[i] ,0,destination[i],0,destination[i].length);
+        }
+        System.out.println(Arrays.deepToString(destination));
     }
 }
